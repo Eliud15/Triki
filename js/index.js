f1e96251cb9215c39c378c03b9f70ae11049fe12
Your Name <you@example.com>
2023-03-27 15:12:33 -0500

    fix bug

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 const box = document.querySelector('.grid-container');
 let value = "X";
 box.addEventListener('click', (event)=>{
-   event.target.innerHTML =`<p class=${value === "O" ? "red" : "lime"}>${value}</p>`;
+    if(event.target.className === 'grid-container') return
+    else {
+        event.target.innerHTML =`<p class=${value === "O" ? "red" : "lime"}>${value}</p>`;
    value === "O" ? value = "X" : value = "O";
     //  if (value === "O") value = "X";
     //  else value = "O"
+    }
 })
