cd61eae9523084098b1ab640c7533d312a08f178
Your Name <you@example.com>
2023-03-28 09:07:34 -0500

    second commit

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 const box = document.querySelector('.grid-container');
+const titulo = document.querySelector('#titulo');
+const info = document.querySelector('.informacion');
+info.style.display = 'none'
+titulo.addEventListener('click',()=> {
+    info.style.display === 'none' ? info.style.display = 'block' : info.style.display = 'none';
+})
 let value = "X";
 box.addEventListener('click', (event)=>{
     if(event.target.className === 'grid-container') return
     else {
-        event.target.innerHTML =`<p class=${value === "O" ? "red" : "lime"}>${value}</p>`;
+        event.target.innerHTML =`<p class=${value === "O" ? "lime" : "red"}>${value}</p>`;
    value === "O" ? value = "X" : value = "O";
     //  if (value === "O") value = "X";
     //  else value = "O"
